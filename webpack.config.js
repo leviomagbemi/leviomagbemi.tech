3234df4609fc9f6a5ac606cb4102e59ca89360c9
Levi Omagbemi <leviomagbemi15@gmail.com>
2024-06-17 21:23:11 +0100

    Added image loader and html loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,18 @@ module.exports = {
       },
 
       {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
